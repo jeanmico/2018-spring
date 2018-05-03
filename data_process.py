fb83aa78fe95fd48ae25e6ad7285781f42931690
Jean <jean@bts-5gvdhjp-dt.ucsf.edu>
2018-05-03 12:43:47 -0700

    filter by multiples, race, ethnicity

--- data_process.py
+++ data_process.py
@@ -7,12 +7,15 @@ filepath = os.path.join(os.path.sep, '/Volumes', 'Padlock', 'TOLSURF')
 mainfile = 'ikaria_pheno_data_ALL_withAncestry.txt'
 headerfile = 'headers.txt'
 multifile = 'tolsurf_twins_iid2.txt'
+outfile = 'filtered_data.txt'
 
 # create header dictionary
+headers = []
 hdr = dict()
 with open(os.path.join(filepath, headerfile), 'r') as f:
     for i, line in enumerate(f.readlines()):
         hdr[line.strip()] = i
+        headers.append(line.strip())
 
 subjects_all = dict()
 with open(os.path.join(filepath, mainfile), 'r') as f:
@@ -25,20 +28,50 @@ if len(subjects_all) != len(set(subjects_all.keys())):
     raise ValueError('there are duplicate patient ids in the file')
 
 # identify subjects of interest
+#TODO: create output file of removed ids
 subjects = dict()
+labid_id = dict()
+na_count = 0
 for key, val in subjects_all.items():
     if val[hdr['APDRACE']] == 'Black or African American':
         subjects[key] = val
+        labid_id[val[hdr['lab_id']]] = key
+        if val[hdr['lab_id']] == 'NA':
+            na_count += 1
 
 print(len(subjects))
+print(len(labid_id))  # 38 subjects for which the ID is NA
+print(na_count)
 
 # remove multiples by id
-remove_ids = set()
+multiples = set()
 with open(os.path.join(filepath, multifile), 'r') as f:
     for line in f:
         linevals = line.strip().split('\t')
-        remove_ids.add(linevals[0])
+        multiples.add(linevals[0])
 
+remove_ids = set()
 for key, val in subjects.items():
-    if val[hdr['lab_id']] in remove_ids:
-        print('multiple')
+    if val[hdr['lab_id']] in multiples:
+        remove_ids.add(val[hdr['lab_id']])
+
+for multiple_id in remove_ids:
+    del subjects[labid_id[multiple_id]]
+# all the multiples were successfully removed
+
+# remove Hispanic/Latino
+hisp_ids = set()
+for key, val in subjects.items():
+    if val[hdr['APDETH']] == 'Hispanic or Latino':
+        hisp_ids.add(key)
+print(len(hisp_ids))
+
+for hisp in hisp_ids:
+    del subjects[hisp]
+
+print(len(subjects))    
+# create output file that can be read into R
+with open(os.path.join(filepath, outfile), 'w+') as out:
+    out.write('\t'.join(str(x) for x in headers))
+    out.write('\n')
+    out.write('\n'.join('\t'.join(str(x) for x in val) for val in subjects.values()))
